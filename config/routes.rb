61cc50de2315af4d6130cc6da88f2882face5f64
Seol Kim <oksusu123@gmail.com>
2018-11-26 19:04:59 -0700

    created routes and started on controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,20 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root route subs is the controller and index is the method name
+  # controller#method name
+  root 'subs#index'
+
+  # get 'subs/new'
+  # get '/turkey', to: 'subs#new'
+
+  # post '/subs', to: 'subs#create'
+  # # :id is a placeholder for the id number
+  # put '/subs/:id', to: 'subs#update'
+  # #:id is used the same way for delete
+  # delete '/subs/:id', to 'subs#delete'
+  # in rails resouces :controller name will take care of the above "get, post, put, delete"
+  
+  resources :subs do
+    resources :topics
+  end
+
 end
